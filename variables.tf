c555da745a1ddf64b1df2335e2406dcfb6c374df
KondapalliPurnaSekhar <36615694+KondapalliPurnaSekhar@users.noreply.github.com>
2024-01-01 16:10:20 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+
+/*
 variable "resource_group_location" {
   default     = "eastus"
   description = "Location of the resource group."
@@ -8,3 +10,4 @@ variable "prefix" {
   default     = "win-vm-iis"
   description = "Prefix of the resource name"
 }
+*/
