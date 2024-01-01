aa2ef3742666a5bc10117d38624f0f3c9cf8f1aa
KondapalliPurnaSekhar <36615694+KondapalliPurnaSekhar@users.noreply.github.com>
2024-01-01 16:11:33 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-
+/*
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = "${random_pet.prefix.id}-rg"
@@ -162,7 +162,7 @@ resource "random_pet" "prefix" {
   length = 1
 }
 
-
+*/
 
 
 
