1f573bdab39c1701bf5b2cc9b53e46d10d8587bd
KondapalliPurnaSekhar <36615694+KondapalliPurnaSekhar@users.noreply.github.com>
2024-01-01 20:05:53 +0530

    outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-/*
 
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
@@ -13,4 +12,4 @@ output "admin_password" {
   value     = azurerm_windows_virtual_machine.main.admin_password
 }
 
-*/
+
